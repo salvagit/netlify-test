82ba10c26ebbb709dd48e4a714d2ac04c34a58bf
salva <spalmiciano@digitalhouse.com>
2020-12-11 01:00:53 -0300

    change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Se lo Digo
         </a>
       </header>
     </div>
